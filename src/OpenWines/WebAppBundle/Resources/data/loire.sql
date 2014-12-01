f5cfdf1420f0a1ac7a8e035628bead86e7df71fe
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-12-01 17:38:09 +0100

    loire data + behat w/ sf2 extention

---
+++ src/OpenWines/WebAppBundle/Resources/data/loire.sql
@@ -0,0 +1,29 @@
+--http://fr.wikipedia.org/wiki/Vignoble_de_la_vall%C3%A9e_de_la_Loire#Vin_blanc
+--http://www.vinsvaldeloire.fr/SiteGP/FR/Appellation/Decouvrez
+--http://www.vinsvaldeloire.fr/uploads/files/V2%20Presse/DPINTERLOIRE2012COMPLET.pdf
+--http://www.vins-centre-loire.com/upload/dossier-presse/vins-centre-loire-fr.pdf
+
+-- Vins d'APPELLATION
+
+-- BLANC:
+`Anjou Blanc`
+`Chinon`
+`Coteaux d`Ancenis`
+`Coteaux du Loir`
+`Gros Plant`
+`Jasnières`
+`Muscadet`
+`Muscadet Coteaux de la Loire`
+`Muscadet Côtes de Grandlieu`
+`Muscadet crus communaux : Clisson, Gorges, Le Pallet`
+`Muscadet Sèvre-et-Maine`
+`Saumur Blanc`
+`Savennières`
+`Savennières Roche aux Moines`
+`Touraine`
+`Touraine Amboise`
+`Touraine Azay-le-Rideau`
+`Touraine Chenonceaux`
+`Touraine Mesland`
+`Touraine Oisly`
+`Vouvray`
