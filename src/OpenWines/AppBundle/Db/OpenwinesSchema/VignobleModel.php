b70d6406cbea0b182ead7c0559dbe46fde9a328a
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-02-09 22:35:45 +0100

    Db classes generated

---
+++ src/OpenWines/AppBundle/Db/OpenwinesSchema/VignobleModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OpenWines\AppBundle\Db\OpenwinesSchema;
+
+use PommProject\ModelManager\Model\Model;
+use PommProject\ModelManager\Model\ModelTrait\WriteQueries;
+use OpenWines\AppBundle\Db\OpenwinesSchema\AutoStructure\Vignoble as VignobleStructure;
+
+/**
+ * VignobleModel
+ *
+ * Model class for table vignoble.
+ *
+ * @see Model
+ */
+class VignobleModel extends Model
+{
+    use WriteQueries;
+
+    /**
+     * __construct()
+     *
+     * Model constructor
+     *
+     * @access public
+     */
+    public function __construct()
+    {
+        $this->structure = new VignobleStructure();
+        $this->flexible_entity_class = '\OpenWines\AppBundle\Db\OpenwinesSchema\Vignoble';
+    }
+}
